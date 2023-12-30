5de94a4642e474f7660db75516c1d0c2b0898c0c
bhushank <bhushank.bwd@gmail.com>
2023-12-30 21:23:15 +0530

    commnet added

---
+++ src/components/Comment.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Comment = () => {
+  const commentData = [
+    {
+      name: "Bhushan",
+      text: "Comment 2",
+      replies: [],
+    },
+    {
+      name: "Sourabh",
+      text: "Comment 3",
+      replies: [
+        {
+          name: "Abhishek",
+          text: "Comment 1",
+          replies: [
+            {
+              name: "manish",
+              text: "Comment 2",
+              replies: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Bhushan",
+      text: "Comment 4",
+      replies: [],
+    },
+    {
+      name: "Bhushan",
+      text: "Comment 5",
+      replies: [
+        {
+          name: "Bhushan",
+          text: "Comment 6",
+          replies: [],
+        },
+      ],
+    },
+  ];
+  const CommentItem = ({ commentDetail }) => {
+    return (
+      <div className="p-2 m-2 bg-slate-200">
+        {commentDetail.name} : {commentDetail.text}
+      </div>
+    );
+  };
+  const CommentList = ({ list }) => {
+    return (
+      <>
+        {list.map((detail, index) => {
+          return (
+            <div key={index}>
+              <CommentItem commentDetail={detail} />
+              {detail.replies && detail.replies.length > 0 && (
+                <div className="p-1 m-7 border-l-2 border-black">
+                  <CommentList list={detail.replies} />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+  return (
+    <div>
+      <CommentList list={commentData} />
+    </div>
+  );
+};
+
+export default Comment;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { hideMenu } from "./utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import Comment from "./Comment";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -11,16 +12,19 @@ const WatchPage = () => {
     dispatch(hideMenu());
   }, []);
   return (
-    <div className="p-2">
-      <iframe
-        width="1000"
-        height="700"
-        src={`https://www.youtube.com/embed/${params.get("v")}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div className="p-2">
+        <iframe
+          width="1000"
+          height="700"
+          src={`https://www.youtube.com/embed/${params.get("v")}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <Comment />
     </div>
   );
 };
